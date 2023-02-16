22b8f9691742aa4e690a3bcd203d636adc217c6e
HeinDk <dekockh@mail.com>
2023-02-16 13:38:16 -0800

    appended return description of main,and arranged code in order

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
 # include <stdio.h>
-
 /**
  * main - Entry point
  *
- * return -Always (0)
+ * Return: Always (0) (Success)
  */
 
 int main(void)
 {
 	printf("Size of a char: %d bytes\n", sizeof(char));
-	printf("Size of float: %d bytes\n", sizeof(float));
-	printf("Size of int: %d bytes\n", sizeof(int));
-	printf("Size of long int: %d bytes\n", sizeof(long int));
-	printf("Size of long long int: %d bytes\n", sizeof(long long int));
+	printf("Size of an int: %d bytes\n", sizeof(int));
+	printf("Size of a long int: %d bytes\n", sizeof(long int));
+	printf("Size of a long long int: %d bytes\n", sizeof(long long int));
+	printf("Size of a float: %d bytes\n", sizeof(float));
 	return (0);
 }

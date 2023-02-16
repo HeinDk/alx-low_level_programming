5d9522ff4497ebac001dd5f0d3d64042ea822973
HeinDk <dekockh@mail.com>
2023-02-16 12:11:34 -0800

    amended errors in code;file 4-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 int main(void)
 {
-	char i[]=("\"Programming is like building a multilingualpuzzle\n");
-	puts(i);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }

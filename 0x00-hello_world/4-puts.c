66e3dbaea8c0412c3809e68e89e3f267d053a1ba
HeinDk <dekockh@mail.com>
2023-02-16 12:26:04 -0800

    added description to code;file 4-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ *4-puts.c
+ *printing strings using puts()
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

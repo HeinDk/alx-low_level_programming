c8ddc8e0f8a14409e719cf169642cf9fc0681059
HeinDk <dekockh@mail.com>
2023-02-16 13:20:18 -0800

    added file 6- size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return -Always (0)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of float: %zu bytes\n", sizeof(float));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of long int: %zu bytes\n", sizeof(long int));
+	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+	return (0);
+}
